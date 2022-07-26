cfbf7a1f6c0f9e33424478c5579edff02326ce2a
안덕우 <fjrlvkdlxj@naver.com>
2022-07-27 00:12:23 +0900

    2022.07.27

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val btn3 = findViewById<Button>(R.id.btn3)
+
+        btn1.text = "a"
+        btn2.text = "b"
+        btn3.text = "n"
+    }
+}
